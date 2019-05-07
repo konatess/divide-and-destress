92a03d72da7fbf829bef4880e394d44525598d89
Kona Tess <konatess@gmail.com>
2019-05-07 15:55:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/divideanddestress/CreateAssignmentActivity.java
@@ -0,0 +1,14 @@
+package com.example.divideanddestress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAssignmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_assignment);
+    }
+}
